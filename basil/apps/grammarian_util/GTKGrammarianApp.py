e9142f77e730615752675edf08e579232f445cce
jon.riehl <jon.riehl@195dae85-c23e-0410-ab0d-ab9888272372>
2008-11-22 00:24:54 +0000

    Added prototypical list boxes and model import functionality to the GTK interface to Grammarian.
    git-svn-id: https://basil.googlecode.com/svn/trunk@45 195dae85-c23e-0410-ab0d-ab9888272372

--- basil/apps/grammarian_util/GTKGrammarianApp.py
+++ basil/apps/grammarian_util/GTKGrammarianApp.py
@@ -15,6 +15,10 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 
+import sys
+import traceback
+
+from basil.models.grammar import GrammarUtils
 from basil.apps.grammarian_util.misc import VERSION
 
 # ______________________________________________________________________
@@ -47,6 +51,34 @@ def make_gtk_menu (menu_obj):
 
 class GTKGramApp (object):
     # ____________________________________________________________
+    def make_list_page (self, prefix, *cols):
+        page = gtk.ScrolledWindow()
+        store = gtk.ListStore(*((str,) * len(cols)))
+        view = gtk.TreeView(store)
+        cell_objs = []
+        col_objs = []
+        for col_name in cols:
+            col = gtk.TreeViewColumn(col_name)
+            view.append_column(col)
+            cell = gtk.CellRendererText()
+            col.pack_start(cell, True)
+            col.add_attribute(cell, "text", len(col_objs))
+            col.set_sort_column_id(len(col_objs))
+            cell_objs.append(cell)
+            col_objs.append(col)
+        view.set_search_column(0)
+        view.set_reorderable(True)
+        page.add(view)
+        view.show()
+        page.show()
+        setattr(self, "%s_page" % prefix, page)
+        setattr(self, "%s_store" % prefix, store)
+        setattr(self, "%s_view" % prefix, view)
+        setattr(self, "%s_cols" % prefix, tuple(col_objs))
+        setattr(self, "%s_cells" % prefix, tuple(cell_objs))
+        return page
+
+    # ____________________________________________________________
     def __init__ (self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_title("Grammarian %s (GTK)" % VERSION)
@@ -73,27 +105,16 @@ class GTKGramApp (object):
         self.vbox.pack_start(self.notebook)
         self.notebook.show()
 
-        self.prod_frame = gtk.Frame("Productions")
-        label = gtk.Label("Production View")
-        self.prod_frame.add(label)
-        label.show()
-        self.prod_frame.show()
-        self.notebook.append_page(self.prod_frame, gtk.Label("Productions"))
+        self.model = None
+        self.model_filename = None
 
-        self.nonterm_frame = gtk.Frame("Nonterminals")
-        label = gtk.Label("Nonterminal View")
-        self.nonterm_frame.add(label)
-        label.show()
-        self.nonterm_frame.show()
-        self.notebook.append_page(self.nonterm_frame,
+        self.notebook.append_page(self.make_list_page("prod", "LHS", "RHS"),
+                                  gtk.Label("Productions"))
+        self.notebook.append_page(self.make_list_page("nonterm",
+                                                      "Nonterminal"),
                                   gtk.Label("Nonterminals"))
-
-        self.term_frame = gtk.Frame("Terminals")
-        label = gtk.Label("Terminal View")
-        self.term_frame.add(label)
-        label.show()
-        self.term_frame.show()
-        self.notebook.append_page(self.term_frame, gtk.Label("Terminals"))
+        self.notebook.append_page(self.make_list_page("term", "Terminal"),
+                                  gtk.Label("Terminals"))
 
         self.statusbar = gtk.Statusbar()
         self.vbox.pack_start(self.statusbar, False, False, 2)
@@ -116,6 +137,26 @@ class GTKGramApp (object):
     def file_open (self, *args):
         if __debug__:
             print "file_open", args
+        self.file_open_dialog = dialog = gtk.FileSelection("Open...")
+        dialog.ok_button.connect("clicked", self.file_open_ok)
+        dialog.cancel_button.connect("clicked", lambda w: dialog.destroy())
+        dialog.show()
+
+    # ____________________________________________________________
+    def file_open_ok (self, *args):
+        if __debug__:
+            print "file_open_ok", args
+        model_filename = self.file_open_dialog.get_filename()
+        try:
+            model = GrammarUtils.getModel(model_filename)
+            self.model_filename = model_filename
+            self.model = model
+            self.update_views()
+        except:
+            print >> sys.stderr, "Failed to open model '%s'." % model_filename
+            print >> sys.stderr, traceback.format_exc()
+        self.file_open_dialog.destroy()
+        self.file_open_dialog = None
 
     # ____________________________________________________________
     def file_save (self, *args):
@@ -137,6 +178,23 @@ class GTKGramApp (object):
     def help_about (self, *args):
         if __debug__:
             print "help_about", args
+
+    # ____________________________________________________________
+    def update_views (self):
+        self.prod_store.clear()
+        self.nonterm_store.clear()
+        self.term_store.clear()
+        start, terms, nonterms, prods = self.model
+        for term in terms:
+            self.term_store.append([term.name])
+        for nonterm in nonterms:
+            self.nonterm_store.append([nonterm.name])
+        for prod in prods:
+            prod = list(prod)
+            lhs, rhs = prod[-2:]
+            lhs_str = " ".join([x.name for x in lhs])
+            rhs_str = " ".join([x.name for x in rhs[0]])
+            self.prod_store.append([lhs_str, rhs_str])
 
     # ____________________________________________________________
     def main (self):
