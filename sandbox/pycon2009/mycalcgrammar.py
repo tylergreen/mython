f57d608309bc681100a9bd65e4edcfe88ee7c00d
tyler.green2 <tyler.green2@195dae85-c23e-0410-ab0d-ab9888272372>
2009-03-31 18:41:49 +0000

    adding PLY calculator language
    git-svn-id: https://basil.googlecode.com/svn/trunk@62 195dae85-c23e-0410-ab0d-ab9888272372

---
+++ sandbox/pycon2009/mycalcgrammar.py
@@ -0,0 +1,59 @@
+#import lex 
+import yacc
+
+
+# import ply.yacc as yacc
+
+# Get the token map from the lexer.  This is required.
+from calclex import tokens
+
+def p_expression_plus(p):
+    'expression : expression PLUS term'
+    p[0] = ('+', [ p[1],p[3]])
+
+def p_expression_minus(p):
+    'expression : expression MINUS term'
+    p[0] = ('-', [ p[1],p[3]])
+
+def p_expression_term(p):
+    'expression : term'
+    p[0] = p[1]
+
+def p_term_times(p):
+    'term : term TIMES factor'
+    p[0] = ('*', [ p[1],p[3]])
+
+def p_term_div(p):
+    'term : term DIVIDE factor'
+    p[0] = ('/', [ p[1],p[3]])
+
+def p_term_factor(p):
+    'term : factor'
+    p[0] = p[1]
+
+def p_factor_num(p):
+    'factor : NUMBER'
+    print type(p[1]), p[1]
+    p[0] = (p[1],[])
+
+def p_factor_expr(p):
+    'factor : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+# Error rule for syntax errors
+def p_error(p):
+    print "Syntax error in input!"
+
+# Build the parser
+parser = yacc.yacc()
+
+if __name__ == '__main__':
+    while True:
+       try:
+           s = raw_input('calc > ')
+       except EOFError:
+           break
+       if not s: continue
+       result = parser.parse(s)
+       print result
+    
